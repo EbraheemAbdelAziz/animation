7fa77452c1bdad9a1119977686f48818174fe62e
Ebrheem Abdel Aziz <ebraheemzezo011@gmail.com>
2023-12-01 23:35:57 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "animate.css";
+import ScrollAnimation from "react-animate-on-scroll";
+import img from "./logo.svg"
+import { useEffect } from "react";
 
+
+
+    
 function App() {
+  useEffect(() => {
+    const elements = document.querySelectorAll(".Shawkat");
+   const optians = {
+     root: null,
+     rootMargin: "0px",
+     threshold: 0.4,
+   };
+   const callbacks = (entries) => {
+     entries.forEach((entry) => {
+       if (entry.isIntersecting) {
+         entry.target.classList.add("ashraf");
+       }
+     });
+   };
+   let observer = new IntersectionObserver(callbacks, optians);
+   elements.forEach((element) => {
+     observer.observe(element);
+   });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* animate__slideInLeft */}
+      <div style={{ fontSize: "100px" }}>dsfhgfkbasfjkdsjkfnas</div>
+      <div style={{ fontSize: "100px" }}>dsfhgfkbasfjkdsjkfnas</div>
+      <div style={{ fontSize: "100px" }}>dsfhgfkbasfjkdsjkfnas</div>
+      <div style={{ fontSize: "100px" }}>dsfhgfkbasfjkdsjkfnas</div>
+      <div style={{ fontSize: "100px" }}>dsfhgfkbasfjkdsjkfnas</div>
+      <div style={{ fontSize: "100px" }}>dsfhgfkbasfjkdsjkfnas</div>
+      <div style={{ fontSize: "100px" }}>dsfhgfkbasfjkdsjkfnas</div>
+      <div style={{ fontSize: "100px" }}>dsfhgfkbasfjkdsjkfnas</div>
+      <div style={{ fontSize: "100px" }}>dsfhgfkbasfjkdsjkfnas</div>
+      <div style={{ fontSize: "100px" }}>dsfhgfkbasfjkdsjkfnas</div>
+      <div style={{ fontSize: "100px" }}>dsfhgfkbasfjkdsjkfnas</div>
+      <div className="Shawkat ">
+        <img src={img} alt="soura" className="soura" />
+      </div>
+      <div className="Shawkat ">
+        <img src={img} alt="soura" className="soura" />
+      </div>
+      <div className="Shawkat ">
+        <img src={img} alt="soura" className="soura" />
+      </div>
+      <div className="Shawkat ">
+        <img src={img} alt="soura" className="soura" />
+      </div>
+    </>
   );
 }
 
